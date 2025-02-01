41cc19aea5e3a198bbd1cdb0abbe06320835c544
phantazeeer <shturmovik729@gmail.com>
2025-02-02 00:01:18 +0400

    created routes

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,9 +1,25 @@
 from fastapi import APIRouter
+from fastapi import Depends
 
-router = APIRouter(tags=["work with users"], prefix="/api")
+from app.api.schemas import UserCreateParameters
+from app.api.schemas import UserCreateResponse
+from app.services import UserService
+from app.utils import IUnitOfWork
+from app.utils import UnitOfWork
+
+router = APIRouter(tags=["work with users"], prefix="/api/auth")
 
 
-# TODO: remove route
-@router.get("/")
-def basic_route():
-    return "basa"
+async def get_user_service(uow: IUnitOfWork = Depends(UnitOfWork)) -> UserService: # noqa
+    return UserService(uow)
+
+
+@router.post("/register")
+async def register(user: UserCreateParameters,
+                   user_service: UserService = Depends(get_user_service)) -> UserCreateResponse:  # noqa
+    """Регистрация пользователя"""
+    access_token, refresh_token = await user_service.register(
+        username=user.name, password=user.password, email=user.email, role=user.role,
+    )
+    response = UserCreateResponse(refresh_token=refresh_token, access_token=access_token, token_type="bearer")
+    return response
