858c4177e3de4963f9a84937d9cb996178154a8a
phantazeeer <shturmovik729@gmail.com>
2025-02-02 00:00:26 +0400

    created schemas

---
+++ app/api/schemas/user_schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+
+
+class UserCreateResponse(BaseModel):
+    access_token: str
+    token_type: str
+    refresh_token: str
+
+
+class UserCreateParameters(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: str
+
+
+class UserLogInParameters(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogInResponse(BaseModel):
+    access_token: str
+    token_type: str
+    refresh_token: str
+
+
+class UserGetMeResponse(BaseModel):
+    user_id: int
+    name: str
+    email: EmailStr
+    is_active: bool
