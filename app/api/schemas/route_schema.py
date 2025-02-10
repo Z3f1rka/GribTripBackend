39bbb3d40884c7ea6ff8e1e21e2d11be8cf1c077
DontAskSillyQuestions <AlinaK73@yandex.ru>
2025-02-10 21:39:06 +0400

    some more schemas

--- app/api/schemas/route_schema.py
+++ app/api/schemas/route_schema.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
@@ -5,11 +7,25 @@ class RouteCreateParameters(BaseModel):
     title: str
 
 
+class RouteGetOne(BaseModel):
+    route_id: int
+    title: str
+    description: str | None = None
+    status: str
+    photo: str | None = None
+    created_at: datetime
+    rating: int
+    main_route_id: int | None = None
+    version: int
+    content_blocks: dict | None = None
+
+
 class RouteUpdateParameters(BaseModel):
     title: str
     description: str | None = None
     photo: str | None = None
     content_blocks: dict | None = None
+    route_id: int
 
 
 class RouteUpdateReturn(BaseModel):
@@ -17,6 +33,8 @@ class RouteUpdateReturn(BaseModel):
     description: str | None = None
     photo: str | None = None
     content_blocks: dict | None = None
+    main_route_id: int
+    route_id: int
 
 
 class ContentBlocks(BaseModel):
