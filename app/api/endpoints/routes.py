e58baca181b9158f0df0b7c3f1fc25e4e8f334b5
phantazeeer <shturmovik729@gmail.com>
2025-02-15 14:06:24 +0400

    added endpoints

--- app/api/endpoints/routes.py
+++ app/api/endpoints/routes.py
@@ -1,25 +1,37 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, status
+from fastapi import Depends
 
 from app.api.schemas import RouteCreateParameters
 from app.api.schemas import RouteReturn
 from app.api.schemas import RouteUpdateParameters
+from app.services.route_service import RouteService
+from app.utils import get_jwt_payload
+from app.utils import IUnitOfWork
+from app.utils import UnitOfWork
 
 router = APIRouter(tags=["Working with map routes"], prefix="/api/routes")
 
-"""async def get_user_service(uow: IUnitOfWork = Depends(UnitOfWork)) -> UserService:  # noqa
-    return None"""
+
+async def get_route_service(uow: IUnitOfWork = Depends(UnitOfWork)) -> RouteService:
+    return RouteService(uow)
 
 
-@router.post("/create")
-async def create(route: RouteCreateParameters):
-    return None
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create(jwt_access: Annotated[str, Depends(get_jwt_payload)], route: RouteCreateParameters,
+                 service: RouteService = Depends(get_route_service)):
+    await service.create_route(int(jwt_access["sub"]), route.title)
 
 
-@router.post("/update")
-async def update(route: RouteUpdateParameters):
-    return None
+@router.post("/update", status_code=status.HTTP_200_OK)
+async def update(jwt_access: Annotated[str, Depends(get_jwt_payload)], route: RouteUpdateParameters,
+                 service: RouteService = Depends(get_route_service)):
+    """В качестве route_id используется main_route_id"""
+    await service.update(route, int(jwt_access["sub"]))
 
 
-@router.get("/get")
-async def get(route) -> RouteReturn:
-    return None
+@router.get("/get_by_id")
+async def get(route_id: int, service: RouteService = Depends(get_route_service)) -> RouteReturn:
+    route = await service.get_route_by_id(route_id)
+    return route
