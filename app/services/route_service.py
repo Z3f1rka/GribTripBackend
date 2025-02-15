5422b245cf06a354701bb896b759a1dfbd681f94
phantazeeer <shturmovik729@gmail.com>
2025-02-15 14:06:10 +0400

    added Create Update to service

--- app/services/route_service.py
+++ app/services/route_service.py
@@ -1,15 +1,33 @@
 from fastapi import HTTPException
 
 from app.api.schemas.route_schema import RouteReturn
+from app.api.schemas.route_schema import RouteUpdateParameters
 from app.utils.unitofwork import IUnitOfWork
+
 
 class RouteService:
     def __init__(self, uow: IUnitOfWork):
         self.uow = uow
 
-    async def get_route(self, id: int):
+    async def get_route_by_id(self, id: int):
         async with self.uow:
-            route = await self.uow.users.find_one(id=id)
-            route = RouteReturn()
+            route = await self.uow.routes.find_one(id=id)
+            route = RouteReturn.model_validate(route)
             return route
-        
+
+    async def create_route(self, user_id: int, title: str):
+        async with self.uow:
+            await self.uow.routes.add_route(user_id=user_id, title=title)
+            await self.uow.commit()
+
+    async def update(self, route: RouteUpdateParameters, user_id: int):
+        async with self.uow:
+            db_route = await self.uow.routes.find_one(main_route_id=route.route_id)
+            if db_route.user_id == user_id:
+                await self.uow.routes.update(title=route.title, description=route.description, photo=route.photo,
+                                             main_route_id=route.route_id, version=route.version,
+                                             content_blocks=route.content_blocks.model_dump())
+                await self.uow.commit()
+            else:
+                raise HTTPException(403, "Пользователь не является владельцем маршрута")
+
