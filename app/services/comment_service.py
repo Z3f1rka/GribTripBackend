c93dddc49ffeda5c18c1f7b355cd9b16e0918b76
DontAskSillyQuestions <AlinaK73@yandex.ru>
2025-02-22 17:58:03 +0400

    users can't comment their own routes

--- app/services/comment_service.py
+++ app/services/comment_service.py
@@ -12,9 +12,11 @@ class CommentService:
     async def create_comment(self, user_id: int, comment: CommentCreateParametrs):
         async with self.uow:
             try:
-                await self.uow.routes.find_one(id=comment.route_id)
+                route = await self.uow.routes.find_one(id=comment.route_id)
             except Exception:
                 raise HTTPException(400, "Маршрут для комментирования не найден")
+            if route.user_id == user_id:
+                raise HTTPException(403, "Маршрут для комментирования не доступен")
             await self.uow.comments.add_comment(user_id=user_id, text=comment.text, rating=comment.rating,
                                                 answer=comment.answer, main_route_id=comment.route_id,
                                                 type=comment.type)
