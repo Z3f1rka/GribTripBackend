c1bbd6b90959d6fa2fb4e89abf61237cb8fe93be
DontAskSillyQuestions <AlinaK73@yandex.ru>
2025-02-22 17:16:32 +0400

    check if comments are real

--- app/services/comment_service.py
+++ app/services/comment_service.py
@@ -11,6 +11,10 @@ class CommentService:
 
     async def create_comment(self, user_id: int, comment: CommentCreateParametrs):
         async with self.uow:
+            try:
+                await self.uow.routes.find_one(id=comment.route_id)
+            except Exception:
+                raise HTTPException(400, "Маршрут для комментирования не найден")
             await self.uow.comments.add_comment(user_id=user_id, text=comment.text, rating=comment.rating,
                                                 answer=comment.answer, route_id=comment.route_id, type=comment.type)
             await self.uow.commit()
@@ -31,7 +35,10 @@ class CommentService:
 
     async def delete_comment(self, user_id: int, comment_id: int):
         async with self.uow:
-            comment = await self.uow.comments.find_one(id=comment_id)
+            try:
+                comment = await self.uow.comments.find_one(id=comment_id)
+            except Exception:
+                raise HTTPException(400, "Такого комментария не существует")
             if comment.user_id != user_id:
                 raise HTTPException(403, "Пользователь не является автором комментария")
             await self.uow.comments.del_one(id=comment_id)
