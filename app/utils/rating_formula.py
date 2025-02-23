abe72fb3da78dfcbbafad1591a8c011a84a42d13
phantazeeer <shturmovik729@gmail.com>
2025-02-23 14:36:20 +0400

    fixed rating_formula.py

--- app/utils/rating_formula.py
+++ app/utils/rating_formula.py
@@ -6,6 +6,6 @@ HFP = settings.HALFLIFE
 
 
 def rating_calculation(rows: list[datetime, int]) -> float:
-    rows = [(1 * 2 ** -((datetime.utcnow() - i[0]).days / HFP), i[1]) for i in rows]  # weight, rating
+    rows = [(1 * 2 ** -(abs((datetime.utcnow() - i[0]).days / HFP)), i[1]) for i in rows]  # weight, rating
     rating = sum(i[0] * i[1] for i in rows) / sum(i[0] for i in rows)
     return rating
