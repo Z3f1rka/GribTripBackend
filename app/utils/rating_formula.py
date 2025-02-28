589442302061dedecd256a08a5fe240a9c4bd878
DontAskSillyQuestions <AlinaK73@yandex.ru>
2025-02-28 19:56:42 +0400

    not float rating

--- app/utils/rating_formula.py
+++ app/utils/rating_formula.py
@@ -8,4 +8,4 @@ HFP = settings.HALFLIFE
 def rating_calculation(rows: list[datetime, int]) -> float:
     rows = [(1 * 2 ** -(abs((datetime.utcnow() - i[0]).days / HFP)), i[1]) for i in rows]  # weight, rating
     rating = sum(i[0] * i[1] for i in rows) / sum(i[0] for i in rows)
-    return rating
+    return int(rating)
