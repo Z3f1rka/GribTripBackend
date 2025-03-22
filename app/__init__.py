578d64473ca878d6eee6e0bbe0cfc4acd9bb3515
PythonGrib <dpdp@gmail.com>
2025-03-22 19:11:57 +0400

    refactored websockets paths

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import admin_router
-from app.api import chat_router
+from app.websockets import ws_chat
 from app.api import comment_router
 from app.api import history_router
 from app.api import route_router
@@ -25,5 +25,5 @@ def create_app() -> FastAPI:
     app.include_router(admin_router)
     app.include_router(comment_router)
     app.include_router(history_router)
-    app.include_router(chat_router)
+    app.include_router(ws_chat)
     return app
---
+++ app/websockets/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from .endpoints.chats import router as ws_chat
